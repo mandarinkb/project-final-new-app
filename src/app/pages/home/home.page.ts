8fec382219dfcb04aba83cbf3daefe8364ba9a90
mandarinkb <mandarinkb@gmail.com>
2020-04-29 20:13:22 +0700

    demo create open  url

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,50 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
+  image: string;
+  name: string;
+  price: number;
+  originalPrice: number;
+  discount: number;
+  icon: string;
+  url: string;
+  category: string;
+
+  listItem: any;
   searchKey = '';
-  constructor(private menuController: MenuController ) { }
+  constructor(private menuController: MenuController,
+              private router: Router ) { }
 
   ngOnInit() {
+    this.listItem = [{
+      image: 'https://secure.ap-tescoassets.com/assets/TH/420/9556001132420/ShotType1_540x540.jpg',
+      name: 'เนสท์เล่ ซีรีแล็ค อินแฟนท์ ซีเรียล ผสมนม อาหารเสริมธัญพืช สูตรข้าวสาลีผสมกล้วยบดและนม 250กรัม',
+      price: 85,
+      originalPrice: 99,
+      discount: 14.1,
+      icon: 'https://www.tescolotus.com/assets/theme2018/tl-theme/img/logo.png',
+      url: 'https://shoponline.tescolotus.com/groceries/th-TH/products/6022001840',
+      category: 'ผลิตภัณฑ์สำหรับเด็ก'
+    },
+    {
+      image: 'https://secure.ap-tescoassets.com/assets/TH/197/8852796103197/ShotType1_540x540.jpg',
+      name: 'พีเดียชัวร์ 1+ คอมพลีท อาหารสูตรครบถ้วนสำหรับเด็กที่เบื่ออาหาร กลิ่นวานิลลา 370กรัม x 6 ซอง',
+      price: 2007,
+      originalPrice: 2231,
+      discount: 10,
+      icon: 'https://www.tescolotus.com/assets/theme2018/tl-theme/img/logo.png',
+      url: 'https://shoponline.tescolotus.com/groceries/th-TH/products/6072716835',
+      category: 'ผลิตภัณฑ์สำหรับเด็ก'
+    }
+  ];
   }
 
-async openMenu() {
-  return await this.menuController.open();
-}
+  async openMenu() {
+    return await this.menuController.open();
+  }
 
+  redirect(pagename: string) {
+    this.router.navigate([pagename]);
+  }
 
 }
